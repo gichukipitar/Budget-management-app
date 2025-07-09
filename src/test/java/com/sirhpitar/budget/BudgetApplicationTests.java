b841ac0fb242d71ab72f2925e56e871f554fec87
gichukipitar <ptahgichuki@yahoo.com>
2025-07-09 20:55:38 +0300

    important updates

--- src/test/java/com/sirhpitar/budget/BudgetApplicationTests.java
+++ src/test/java/com/sirhpitar/budget/BudgetApplicationTests.java
@@ -2,12 +2,15 @@ package com.sirhpitar.budget;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BudgetApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        //test to ensure the application context loads successfully
+    }
 
 }
